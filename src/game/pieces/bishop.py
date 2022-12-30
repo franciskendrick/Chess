83ab81e8b2506a21e906fecd67cf485062c01a37
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-12-30 14:52:34 +0800

    Added bishop's valid moves

--- src/game/pieces/bishop.py
+++ src/game/pieces/bishop.py
@@ -12,4 +12,70 @@ class Bishop(Piece):
         self.offset = (3, 1)
 
     def valid_moves(self, board):
-        pass
+        row = self.row
+        col = self.col
+
+        valid_moves = []
+
+        # Top-right
+        x = col + 1
+        for y in range(row - 1, -1, -1):
+            if x <= 7:
+                square = board[y][x]
+                if square == 0:  # if an empty square
+                    valid_moves.append((x, y))
+                elif square.color != self.color:  # if there is a opponent piece
+                    valid_moves.append((x, y))
+                    break
+                else:  # if there is an ally piece
+                    break
+                
+            x += 1
+        
+        # Top-left
+        x = col - 1
+        for y in range(row - 1, -1, -1):
+            if x >= 0:
+                square = board[y][x]
+                if square == 0:  # if an empty square
+                    valid_moves.append((x, y))
+                elif square.color != self.color:  # if there is a opponent piece
+                    valid_moves.append((x, y))
+                    break
+                else:  # if there is an ally piece
+                    break
+
+            x -= 1
+
+        # Bottom-right
+        x = col + 1
+        for y in range(row + 1, 8):
+            if x <= 7:
+                square = board[y][x]
+                if square == 0:  # if an empty square
+                    valid_moves.append((x, y))
+                elif square.color != self.color:  # if there is a opponent piece
+                    valid_moves.append((x, y))
+                    break
+                else:  # if there is an ally piece
+                    break
+
+            x += 1
+        
+        # Bottom-Left
+        x = col - 1
+        for y in range(row + 1, 8):
+            if x >= 0:
+                square = board[y][x]
+                if square == 0:  # if an empty square
+                    valid_moves.append((x, y))
+                elif square.color != self.color:  # if there is a opponent piece
+                    valid_moves.append((x, y))
+                    break
+                else:  # if there is an ally piece
+                    break
+
+            x -= 1
+
+        # Return
+        return valid_moves
