07ddc2e48b1cefb052b0f2a8048f0b5b857ed731
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2023-01-18 22:23:22 +0800

    Added king's valid moves

--- src/game/pieces/king.py
+++ src/game/pieces/king.py
@@ -12,4 +12,77 @@ class King(Piece):
         self.offset = (1, 1)
 
     def valid_moves(self, board):
-        pass
+        row = self.row
+        col = self.col
+
+        valid_moves = []
+
+        # Top
+        if row > 0:
+            # Top Left
+            if col > 0:
+                square = board[row - 1][col - 1]
+                if square == 0:
+                    valid_moves.append((col - 1, row - 1))
+                elif square.color != square.color:
+                    valid_moves.append((col - 1, row - 1))
+
+            # Top Middle
+            square = board[row - 1][col]
+            if square == 0:
+                valid_moves.append((col, row - 1))
+            elif square.color != square.color:
+                valid_moves.append((col, row - 1))
+
+            # Top Right
+            if col < 7:
+                square = board[row - 1][col + 1]
+                if square == 0:
+                    valid_moves.append((col + 1, row - 1))
+                elif square.color != square.color:
+                    valid_moves.append((col + 1, row - 1))
+
+
+        # Bottom
+        if row < 7:
+            # Bottom Left
+            if col > 0:
+                square = board[row + 1][col - 1]
+                if square == 0:
+                    valid_moves.append((col - 1, row + 1))
+                elif square.color != square.color:
+                    valid_moves.append((col - 1, row + 1))
+
+            # Bottom Middle
+            square = board[row + 1][col]
+            if square == 0:
+                valid_moves.append((col, row + 1))
+            elif square.color != square.color:
+                valid_moves.append((col, row + 1))
+
+            # Bottom Right
+            if col < 7:
+                square = board[row + 1][col + 1]
+                if square == 0:
+                    valid_moves.append((col + 1, row + 1))
+                elif square.color != square.color:
+                    valid_moves.append((col + 1, row + 1))
+
+        # left Middle
+        if col > 0:
+            square = board[row][col - 1]
+            if square == 0:
+                valid_moves.append((col - 1, row))
+            elif square.color != square.color:
+                valid_moves.append((col - 1, row))
+
+        # Right Middle
+        if col < 7:
+            square = board[row][col + 1]
+            if square == 0:
+                valid_moves.append((col + 1, row))
+            elif square.color != square.color:
+                valid_moves.append((col + 1, row))
+
+        # Return
+        return valid_moves
